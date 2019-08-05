6adf9ce9541efd76609fe125748b504f1889ca85
Fred Snyder <fsnyder@castwide.com>
2019-08-04 22:02:50 -0400

    Hack/todo

--- lib/readapt/message/disconnect.rb
+++ lib/readapt/message/disconnect.rb
@@ -4,6 +4,9 @@ module Readapt
   module Message
     class Disconnect < Base
       def run
+        # HACK: Wait a moment to make sure the output is flushed
+        # @todo Find a better way
+        sleep 1
         Monitor.stop
         Backport.stop if inspector.debugger.launched?
       end
