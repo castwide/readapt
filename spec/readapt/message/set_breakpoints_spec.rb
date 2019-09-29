355dff69d615dbbb85cd8943e8f40e9642dfa6ba
Fred Snyder <fsnyder@castwide.com>
2019-09-29 19:31:47 -0400

    SetBreakpoints spec mocks debugger.

--- spec/readapt/message/set_breakpoints_spec.rb
+++ spec/readapt/message/set_breakpoints_spec.rb
@@ -1,11 +1,12 @@
 RSpec.describe Readapt::Message::SetBreakpoints do
   it 'sets breakpoints' do
+    debugger = double(:Debugger, clear_breakpoints: nil, set_breakpoint: nil)
     message = Readapt::Message::SetBreakpoints.new({
       'source' => {
         'path' => 'test.rb'
       },
       'lines' => [1]
-    }, nil)
+    }, debugger)
     message.run
     expect(message.body[:breakpoints]).not_to be_nil
     expect(Readapt::Breakpoints.match('test.rb', 1)).to be(true)
