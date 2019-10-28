3109042ed8e79d2ce2b99f4a5155d9e1ee0e128a
Fred Snyder <fsnyder@castwide.com>
2019-10-28 17:08:05 -0400

    Unused functions.

--- ext/readapt/inspector.c
+++ ext/readapt/inspector.c
@@ -4,28 +4,6 @@
 #include "threads.h"
 #include "normalize.h"
 
-static size_t
-inspector_size(const void *data)
-{
-    return sizeof(void *);
-}
-
-static const rb_data_type_t inspector_type = {
-    "inspector",
-    {0, 0, inspector_size,},
-};
-
-static const rb_debug_inspector_t *
-rb_debug_inspector_from_location(VALUE self)
-{
-    const rb_debug_inspector_t *dc;
-    TypedData_Get_Struct(self, const rb_debug_inspector_t, &inspector_type, dc);
-    if (dc == 0) {
-    	rb_raise(rb_eArgError, "invalid debug context");
-    }
-    return dc;
-}
-
 static VALUE process_inspection(const rb_debug_inspector_t *inspector, void *ptr)
 {
     VALUE locations;
@@ -49,15 +27,8 @@ static VALUE process_inspection(const rb_debug_inspector_t *inspector, void *ptr
         loc = rb_ary_entry(locations, i);
         path = rb_funcall(loc, rb_intern("absolute_path"), 0);
         line = NUM2INT(rb_funcall(loc, rb_intern("lineno"), 0));
-
         bnd = rb_debug_inspector_frame_binding_get(inspector, i);
 
-        // frm = data->frames->elements[cursor];
-        // if (frm->line == line && strcmp(frm->file, StringValueCStr(path)) == 0)
-        // {
-        //     frm->binding = bnd;
-        //     cursor--;
-        // }
         frm = malloc(sizeof(frame_t));
         frm->file = normalize_path_new_cstr(StringValueCStr(path));
         frm->line = line;
