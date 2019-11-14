c2d0ae7cd27cc000d35e0f8252a154b89bb44414
Fred Snyder <fsnyder@castwide.com>
2019-11-14 14:50:17 -0500

    Header name conflicts in MacOS (#4)

--- ext/readapt/stack.h
+++ ext/readapt/stack.h
@@ -3,18 +3,18 @@
 
 #include "stddef.h"
 
-typedef struct stack_struct {
+typedef struct readapt_stack_struct {
     int size;
     size_t elem_size;
     void (*free_func)(void *);
     int capacity;
     void **elements;
-} stack_t;
+} readapt_stack_t;
 
-stack_t *stack_alloc(size_t elem_size, void(*free_func)(void*));
-void stack_push(stack_t *stack, void *element);
-void *stack_peek(stack_t *stack);
-void stack_pop(stack_t *stack);
-void stack_free(stack_t *stack);
+readapt_stack_t *stack_alloc(size_t elem_size, void(*free_func)(void*));
+void stack_push(readapt_stack_t *stack, void *element);
+void *stack_peek(readapt_stack_t *stack);
+void stack_pop(readapt_stack_t *stack);
+void stack_free(readapt_stack_t *stack);
 
 #endif
