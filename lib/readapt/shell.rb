45948d8ad4f9cd306fa456d8afffa9bb15c5bf1c
Fred Snyder <fsnyder@castwide.com>
2019-11-17 13:07:39 -0500

    Rename serve to socket.

--- lib/readapt/shell.rb
+++ lib/readapt/shell.rb
@@ -13,62 +13,29 @@ module Readapt
       puts Readapt::VERSION
     end
 
-    desc 'serve', 'Run a DAP server'
+    desc 'socket', 'Run a DAP server over TCP'
     option :host, type: :string, aliases: :h, description: 'The server host', default: '127.0.0.1'
     option :port, type: :numeric, aliases: :p, description: 'The server port', default: 1234
-    def serve
-      STDOUT.sync = true
-      STDERR.sync = true
+    def socket
       machine = Backport::Machine.new
       machine.run do
-        Signal.trap("INT") do
-          graceful_shutdown
-        end
-        Signal.trap("TERM") do
-          graceful_shutdown
-        end
-        procid = SecureRandom.hex(8)
-        Readapt::Error.procid = procid
-        stdin, stdout, stderr = Open3.popen3('ruby', $0, 'target', procid)
-        stdin.sync = true
-        stdout.sync = true
-        stderr.sync = true
-        stdin.binmode
-        Readapt::Server.target_in = stdin
-        output = Backport::Server::Stdio.new(input: stdout, output: stdin, adapter: Readapt::Output)
-        error = Backport::Server::Stdio.new(input: stderr, output: stdin, adapter: Readapt::Error)
+        prepare_machine machine
         server = Backport::Server::Tcpip.new(host: options[:host], port: options[:port], adapter: Readapt::Server)
-        machine.prepare output
-        machine.prepare error
         machine.prepare server
         STDERR.puts "Readapt Debugger #{Readapt::VERSION} is listening HOST=#{options[:host]} PORT=#{options[:port]} PID=#{Process.pid}"
-        STDERR.flush
       end
     end
+    map serve: :socket
 
-    # desc 'stdio', 'Run a DAP process'
-    # def stdio
-    #   STDOUT.sync = true
-    #   STDERR.sync = true
-    #   machine = Backport::Machine.new
-    #   machine.run do
-    #     Signal.trap("INT") do
-    #       graceful_shutdown
-    #     end
-    #     Signal.trap("TERM") do
-    #       graceful_shutdown
-    #     end
-    #     procid = SecureRandom.hex(8)
-    #     Readapt::Error.procid = procid
-    #     stdin, stdout, stderr = Open3.popen3('ruby', $0, 'target', procid)
-    #     STDERR.puts "Readapt Debugger #{Readapt::VERSION} is running"
-    #     # machine.prepare Backport::Server::Stdio.new(input: STDIN, output: stdin, adapter: Readapt::Input)
-    #     # machine.prepare Backport::Server::Stdio.new(input: stdout, output: STDOUT, adapter: Readapt::Output)
-    #     machine.prepare Backport::Server::Stdio.new(input: STDIN, output: stdin, adapter: Readapt::Input)
-    #     machine.prepare Backport::Server::Stdio.new(input: stdout, output: STDOUT, adapter: Readapt::Output)
-    #     machine.prepare Backport::Server::Stdio.new(input: stderr, output: STDOUT, adapter: Readapt::Error)
-    #   end
-    # end
+    desc 'stdio', 'Run a DAP server over STDIO'
+    def stdio
+      machine = Backport::Machine.new
+      machine.run do
+        prepare_machine machine
+        server = Backport::Server::Stdio.new(adapter: Readapt::Server)
+        machine.prepare server
+      end
+    end
 
     desc 'target [PROCID]', 'Run a target process'
     def target procid = nil
@@ -93,6 +60,29 @@ module Readapt
     end
 
     private
+
+    def prepare_machine machine
+      STDOUT.sync = true
+      STDERR.sync = true
+      Signal.trap("INT") do
+        graceful_shutdown
+      end
+      Signal.trap("TERM") do
+        graceful_shutdown
+      end
+      procid = SecureRandom.hex(8)
+      Readapt::Error.procid = procid
+      stdin, stdout, stderr = Open3.popen3('ruby', $0, 'target', procid)
+      stdin.sync = true
+      stdout.sync = true
+      stderr.sync = true
+      stdin.binmode
+      Readapt::Server.target_in = stdin
+      output = Backport::Server::Stdio.new(input: stdout, output: stdin, adapter: Readapt::Output)
+      error = Backport::Server::Stdio.new(input: stderr, output: stdin, adapter: Readapt::Error)
+      machine.prepare output
+      machine.prepare error
+    end
 
     def graceful_shutdown
       Backport.stop
